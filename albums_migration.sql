3ad78c383fef4432b97d9b43060e55d68628c8de
Darius Brown <Brown.darius.r@gmail.com>
2021-05-18 12:27:18 -0500

    added 2 more albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,7 +2,7 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
---create album table
+-- create album table
 CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50),
@@ -12,9 +12,18 @@ CREATE TABLE IF NOT EXISTS albums (
     genre VARCHAR(50),
     PRIMARY KEY (id)
 );
---insert data to table
+-- insert data to table
 INSERT INTO albums (
    id, artist, name, release_date, sales, genre)
-   VALUES ('1', 'DB THE MUSICIAN', 'Darius Brown', '1994', '5000', 'JAZZ METAL');
---Selects and shows albums table
+   VALUES ('1', 'DB THE MUSICIAN', 'Darius Brown', '1994', '5000', 'JAZZ METAL'
+   );
+INSERT INTO albums (
+    id, artist, name, release_date, sales, genre)
+VALUES ('2', '2pac', 'Still I Rise', '1998', '408000', 'posthumous HIP-HOP'
+       );
+INSERT INTO albums (
+    id, artist, name, release_date, sales, genre)
+VALUES ('3', 'Mac Demarco', 'On The Level', '2017', '20000', 'SOFT ROCK'
+       );
+-- Selects and shows albums table
 SELECT * FROM albums;
