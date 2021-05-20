5b68bb7ae39fd9850ff46dd536ab996621c1cf33
Darius Brown <Brown.darius.r@gmail.com>
2021-05-20 13:55:01 -0500

    finished orderBy

---
+++ order_by_exercises.sql
@@ -0,0 +1,16 @@
+USE employees;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name DESC;
+SELECT * FROM employees WHERE last_name LIKE 'e%' ORDER BY emp_no DESC;
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Maya' OR first_name = 'Vidya'
+    AND gender LIKE 'M';
+
+SELECT * FROM employees WHERE first_name LIKE 'e%' OR first_name LIKE 'e%';
+
+SELECT * FROM employees WHERE first_name LIKE 'e%' AND first_name LIKE 'e%';
+
+SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%qu%';
+
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
