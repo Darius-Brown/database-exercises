1d6c0087df4e4b2c772287bd47b07ffff6f3e4ae
Darius Brown <Brown.darius.r@gmail.com>
2021-05-20 12:12:39 -0500

    going to lunch

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,10 +5,12 @@ SELECT * FROM employees WHERE last_name LIKE 'e%';
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Maya' OR first_name = 'Vidya'
-AND gender NOT LIKE 'M';
+AND gender LIKE 'M';
 
 SELECT * FROM employees WHERE first_name LIKE 'e%' OR first_name LIKE 'e%';
 
 SELECT * FROM employees WHERE first_name LIKE 'e%' AND first_name LIKE 'e%';
 
-Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%qu%';
+
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
