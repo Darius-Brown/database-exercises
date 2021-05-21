57240fa11c7a1a64930f99d334504f265e8554c5
Darius Brown <Brown.darius.r@gmail.com>
2021-05-21 12:10:39 -0500

    starting 7

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,3 +6,17 @@ SELECT last_name FROM employees
 WHERE last_name LIKE 'e%'
 AND last_name LIKE '%e'
 GROUP BY last_name;
+
+SELECT last_name FROM employees
+WHERE last_name LIKE 'e%'
+AND last_name LIKE '%e'
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT COUNT(last_name), last_name FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
