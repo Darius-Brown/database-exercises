e98facf4cdfb3739e26d356cd30ad12350f3c16c
Darius Brown <Brown.darius.r@gmail.com>
2021-05-21 12:14:21 -0500

    finished

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -11,12 +11,10 @@ SELECT last_name FROM employees
 WHERE last_name LIKE 'e%'
 AND last_name LIKE '%e'
 
-SELECT last_name FROM employees
-WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%'
-GROUP BY last_name;
-
 SELECT COUNT(last_name), last_name FROM employees
 WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
+
+SELECT COUNT(first_name), gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY first_name, gender;
