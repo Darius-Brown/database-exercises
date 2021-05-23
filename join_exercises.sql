9d3e58849daa0c5fddd0b586c0e343944cf80348
Darius Brown <Brown.darius.r@gmail.com>
2021-05-23 00:35:47 -0500

    making progress

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,7 +1,15 @@
 USE employees;
 
-SELECT d.dept_name FROM employees as e
-         JOIN dept_manager as dm
-            ON dm.emp_no = e.emp_no
-        JOIN departments as d
-            ON d.dept_no = dm.dept_no;
+-- SELECT d.dept_name, e.first_name FROM employees
+--          JOIN dept_manager as dm
+--             ON dm.emp_no = e.emp_no
+--         JOIN departments as d
+--             ON d.dept_no = dm.dept._no;
+--         JOIN employees AS e
+--
+
+SELECT e.first_name, d.dept_name FROM employees as e
+JOIN dept_manager as dm
+ON dm.emp_no = e.emp_no
+JOIN departments as d
+ON d.dept_no = dm.dept_no;
