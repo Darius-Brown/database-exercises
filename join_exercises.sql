f0532c8133ec60d3b478a2511e024baf9f9c5da9
Darius Brown <Brown.darius.r@gmail.com>
2021-05-23 01:27:35 -0500

    finished second question

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,13 +1,5 @@
 USE employees;
 
--- SELECT d.dept_name, e.first_name FROM employees
---          JOIN dept_manager as dm
---             ON dm.emp_no = e.emp_no
---         JOIN departments as d
---             ON d.dept_no = dm.dept._no;
---         JOIN employees AS e
---
-
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, d.dept_name AS Department_Name
 FROM employees AS e
 
@@ -18,5 +10,18 @@ JOIN departments AS d
 ON d.dept_no = dm.dept_no
 
 WHERE dm.to_date = '9999-01-01';
+
+-- Managed by women
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager, d.dept_name AS Department_Name
+FROM employees AS e
+
+         JOIN dept_manager AS dm
+              ON dm.emp_no = e.emp_no
+
+         JOIN departments AS d
+              ON d.dept_no = dm.dept_no
+
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
+
 
 
