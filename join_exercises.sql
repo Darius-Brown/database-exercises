43a89b1949f66cecdf88c6f2a669c9a86c1be2b8
Darius Brown <Brown.darius.r@gmail.com>
2021-05-23 00:19:41 -0500

    sunday commit' git push origin main

--- join_exercises.sql
+++ join_exercises.sql
@@ -4,4 +4,4 @@ SELECT d.dept_name FROM employees as e
          JOIN dept_manager as dm
             ON dm.emp_no = e.emp_no
         JOIN departments as d
-            ON d.dept_no = dm.dept_no
+            ON d.dept_no = dm.dept_no;
