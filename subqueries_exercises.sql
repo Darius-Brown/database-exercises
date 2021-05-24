b49422ed6e72130dfb313ef56d5dd7cebed30f0f
Darius Brown <Brown.darius.r@gmail.com>
2021-05-24 12:27:57 -0500

    finished bonus, nested subqueries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -20,3 +20,14 @@ WHERE emp_no IN (
     FROM dept_manager
     WHERE gender = 'F' AND to_date = '9999-01-01'
     );
+
+SELECT dept_name FROM departments
+WHERE dept_no IN (
+        SELECT dept_no
+        FROM dept_manager
+        WHERE emp_no IN (
+            SELECT emp_no
+            FROM employees
+            WHERE gender = 'F'
+              AND to_date = '9999-01-01'
+        ));
